4cc1beace91a6be40d90a68ee23e281b2969689b
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2023-06-19 17:47:34 +0100

    add saving path logging

--- src/irc_kaggle/utils/core.py
+++ src/irc_kaggle/utils/core.py
@@ -3,12 +3,13 @@ import pickle
 
 
 def save_cv_grid(dir_path, tuned_grid_searches, prefix=None):
-
     for clf_name, tuned_grid in tuned_grid_searches.items():
         if prefix:
             pickle_name = f"{prefix}_{clf_name}_tuned_grid.pkl"
         else:
             pickle_name = f"{clf_name}_tuned_grid.pkl"
 
-        with open(os.path.join(dir_path, pickle_name), 'wb') as f:
+        path = os.path.join(dir_path, pickle_name)
+        print(f"Saving as {path}")
+        with open(path, 'wb') as f:
             pickle.dump(tuned_grid, f)
