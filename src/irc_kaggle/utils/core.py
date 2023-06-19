dc9ecef5496892267fc890994cb7ab14ccba1af5
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2023-06-19 15:26:12 +0100

    fix bug in saving the object

--- src/irc_kaggle/utils/core.py
+++ src/irc_kaggle/utils/core.py
@@ -3,11 +3,12 @@ import pickle
 
 
 def save_cv_grid(dir_path, tuned_grid_searches, prefix=None):
-    for tuned_grid in tuned_grid_searches:
+
+    for clf_name, tuned_grid in tuned_grid_searches.items():
         if prefix:
-            pickle_name = f"{prefix}_{tuned_grid['clf_name']}_tuned_grid.pkl"
+            pickle_name = f"{prefix}_{clf_name}_tuned_grid.pkl"
         else:
-            pickle_name = f"{tuned_grid['clf_name']}_tuned_grid.pkl"
+            pickle_name = f"{clf_name}_tuned_grid.pkl"
 
         with open(os.path.join(dir_path, pickle_name), 'wb') as f:
             pickle.dump(tuned_grid, f)
